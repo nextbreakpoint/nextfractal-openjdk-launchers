2d2916ef99d16f2e7b4265f7a70ac2a794d117b9
Andrea Medeghini <andrea@nextbreakpoint.com>
2020-02-16 00:01:47 +0000

    move files to dist

--- package.sh
+++ package.sh
@@ -6,3 +6,10 @@ sh -c "cd osx/build && zip -9 osx-nextfractal-launcher-$VERSION.zip NextFractal"
 sh -c "cd debian/build && zip -9 debian-nextfractal-launcher-$VERSION.zip NextFractal"
 sh -c "cd fedora/build && zip -9 fedora-nextfractal-launcher-$VERSION.zip NextFractal"
 sh -c "cd windows/build && zip -9 windows-nextfractal-launcher-$VERSION.zip NextFractal.exe"
+
+mkdir -p dist
+
+cp osx/build/osx-nextfractal-launcher-$VERSION.zip dist
+cp debian/build/debian-nextfractal-launcher-$VERSION.zip dist
+cp fedora/build/fedora-nextfractal-launcher-$VERSION.zip dist
+cp windows/build/windows-nextfractal-launcher-$VERSION.zip dist
